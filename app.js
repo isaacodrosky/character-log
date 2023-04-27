1b8c0dfe4c8e31be6d54b62b7534b33fd2b18221
isaacodrosky <isaacodrosky@gmail.com>
2023-04-27 10:50:30 -0400

    Added character details
    Can now track when character was first mentioned, description, location, and any notes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-
+// variables
 const saveBtn = document.getElementById('save-btn');
 const characterForm = document.getElementById('character-form');
 const characterSection = document.getElementById('character-section');
 const characterNameInput = document.getElementById('character-name-input');
 const bookNameInput = document.getElementById('book-name-input');
+const firstMentionInput = document.getElementById('first-mention-input');
+const descriptionInput = document.getElementById('description-input');
+const locationInput = document.getElementById('location-input');
+const notesInput = document.getElementById('notes-input');
+
 let charactersArr = [];
 
+// event listeners
 saveBtn.addEventListener('click', renderCharacter);
 
 // display character array in browser
@@ -26,6 +32,10 @@ function addNewCharacter() {
     <div class="character-card">
        <h2>${character.name}</h2>
        <p>${character.book}</p>
+       <p>First mentioned: ${character.firstMention}</p>
+       <p>Description: ${character.description}</p>
+       <p>Location: ${character.location}</p>
+       <p>Notes: ${character.notes}</p>
      </div>
     `;
   }
@@ -38,13 +48,26 @@ function retrieveInput() {
   const characterFormData = new FormData(characterForm);
   const characterName = characterFormData.get('character-name-input');
   const bookName = characterFormData.get('book-name-input');
+  const firstMention = characterFormData.get('first-mention-input');
+  const description = characterFormData.get('description-input');
+  const location = characterFormData.get('location-input');
+  const notes = characterFormData.get('notes-input');
   // create character object from user input
   const characterObj = {
     name: characterName,
-    book: bookName
+    book: bookName,
+    firstMention: firstMention,
+    description: description,
+    location: location,
+    notes: notes,
   };
   // clear input fields
   characterNameInput.value = "";
   bookNameInput.value = "";
+  firstMentionInput.value = "";
+  descriptionInput.value = "";
+  locationInput.value = "";
+  notesInput.value = "";
+  
   return characterObj;
 }
