06fd86e73cf42f7578876c47193423e5b91bd6d3
isaacodrosky <isaacodrosky@gmail.com>
2023-05-17 13:59:58 -0400

    Refactor app.js

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 // uuid generator
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-// variables
-const addBtn = document.getElementById('add-btn');
-const characterForm = document.getElementById('character-form');
-const characterSection = document.getElementById('character-section');
 const characterNameInput = document.getElementById('character-name-input');
-const bookNameInput = document.getElementById('book-name-input');
-const firstMentionInput = document.getElementById('first-mention-input');
-const descriptionInput = document.getElementById('description-input');
-const locationInput = document.getElementById('location-input');
-const notesInput = document.getElementById('notes-input');
-
+const characterSection = document.getElementById('character-section');
 let renderHtml;
 let charactersArr = [];
-
-// retrieve characters from local storage
 let charactersFromLocalStorage = JSON.parse(localStorage.getItem("myCharacters"));
-// if there are characters in local storage, add them to the character array and render them to the DOM
+
+// if there are characters in local storage, add them to the character array and render to the DOM
 if (charactersFromLocalStorage) {
   charactersArr = charactersFromLocalStorage;
   characterSection.innerHTML = getRenderHtml();
@@ -26,9 +16,9 @@ if (charactersFromLocalStorage) {
 document.addEventListener('click', function(e){
   if (e.target.dataset.delete) { 
     handleDeleteClick(e.target.dataset.delete) 
-  } else if (e.target === addBtn) {
+  } else if (e.target === document.getElementById('add-btn')) {
     if (characterNameInput.value.length > 0) {
-      renderCharacter(e);
+      renderNewCharacter(e);
     } else {
       alert('Please enter a character name.');
     }
@@ -39,15 +29,14 @@ document.addEventListener('click', function(e){
   }
 })
 
-// delete character from array
 function handleDeleteClick(characterId) {
-  charactersArr.forEach(function(character){ // iterate thru characters array
-    if (character.uuid === characterId) { // find character object with same uuid as target
+  charactersArr.forEach(function(character){ 
+    if (character.uuid === characterId) { 
       if (confirm(`Are you sure you want to delete ${character.name}?`)){ // ask user to confirm they want to delete this character
         let targetCharacterObj = character; 
         charactersArr.splice(charactersArr.indexOf(targetCharacterObj), 1); // remove targetCharacterObj from array
-        localStorage.setItem("myCharacters", JSON.stringify(charactersArr)); // update local storage
-        characterSection.innerHTML = getRenderHtml(); // update rendered html
+        localStorage.setItem("myCharacters", JSON.stringify(charactersArr)); 
+        characterSection.innerHTML = getRenderHtml(); 
       }
     }
   })
@@ -94,9 +83,9 @@ function handleSaveClick(characterId) {
   })
 }
 
-// display character array in browser
-function renderCharacter(e) {
-  e.preventDefault(); // prevent refresh on submit
+// display new character array in browser
+function renderNewCharacter(e) {
+  e.preventDefault();
   charactersArr = retrieveInput()
   characterSection.innerHTML = getRenderHtml();
 }
@@ -106,13 +95,25 @@ function getRenderHtml() {
   for (let character of charactersArr) {
     renderHtml += `
     <div class="character-card" id="${character.uuid}">
-       <h2><span class="character-detail" data-name="${character.uuid}">${character.name}</span></h2>
-       <h3><span class="character-detail" data-book="${character.uuid}">${character.book}</span></h3>
-       <p>First mentioned (page): <span class="character-detail" data-first-mention="${character.uuid}">${character.firstMention}</span></p>
-       <p>Description: <span class="character-detail" data-description="${character.uuid}">${character.description}</span></p>
-       <p>Location: <span class="character-detail" data-location="${character.uuid}">${character.location}</span></p>
-       <p>Notes: <span class="character-detail" data-notes="${character.uuid}">${character.notes}</span></p>
-       <p class="icons">
+      <h2>
+        <span class="character-detail" data-name="${character.uuid}">${character.name}</span>
+      </h2>
+      <h3>
+        <span class="character-detail" data-book="${character.uuid}">${character.book}</span>
+      </h3>
+      <p>First mentioned (page): 
+        <span class="character-detail" data-first-mention="${character.uuid}">${character.firstMention}</span>
+      </p>
+      <p>Description: 
+        <span class="character-detail" data-description="${character.uuid}">${character.description}</span>
+      </p>
+      <p>Location: 
+        <span class="character-detail" data-location="${character.uuid}">${character.location}</span>
+      </p>
+      <p>Notes: 
+        <span class="character-detail" data-notes="${character.uuid}">${character.notes}</span>
+      </p>
+      <p class="icons">
         <i class="fa fas fa-pencil edit-btn" data-edit="${character.uuid}"></i>
         <i class="fa fa-solid fa-trash delete-btn" data-delete="${character.uuid}"></i>
       </p> 
@@ -122,11 +123,10 @@ function getRenderHtml() {
   return renderHtml;
 }
 
-// build new character object from user form input
+// build new character object from user form input & save to local storage
 function retrieveInput() {
-  
-    const characterFormData = new FormData(characterForm);
-    // build character object from user input
+
+    const characterFormData = new FormData(document.getElementById('character-form'));
     const characterObj = {
       name: characterFormData.get('character-name-input'),
       book: characterFormData.get('book-name-input'),
@@ -137,18 +137,15 @@ function retrieveInput() {
       uuid: uuidv4(),
       };
 
-  // add new character object to array
   charactersArr.push(characterObj);
 
-  // clear input fields
   characterNameInput.value = "";
-  bookNameInput.value = "";
-  firstMentionInput.value = "";
-  descriptionInput.value = "";
-  locationInput.value = "";
-  notesInput.value = "";
+  document.getElementById('book-name-input').value = "";
+  document.getElementById('first-mention-input').value = "";
+  document.getElementById('description-input').value = "";
+  document.getElementById('location-input').value = "";
+  document.getElementById('notes-input').value = "";
   
-  // save array to local storage
   localStorage.setItem("myCharacters", JSON.stringify(charactersArr));
 
   return charactersArr;
